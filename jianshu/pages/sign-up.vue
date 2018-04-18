9ea242f8fabcf42b22f41e0286279e31906c4593
dlapple <dlapple123>
2018-04-18 19:13:43 +0800

    登录页面

--- jianshu/pages/sign-up.vue
+++ jianshu/pages/sign-up.vue
@@ -2,6 +2,24 @@
     <p>注册</p>
 </template>
 <script>
-    
-
+export default {
+  // 修改当前页面的默认模板为sign.vue
+  // 如果不写的话，默认的是default.vue
+  layout: "sign",
+  // 设置页面的标题以及描述、关键字等元素
+  head: {
+    title: "注册-简书",
+    meta: [
+      { charst: "utf-8" },
+      { name: "keywords", content: "注册页面" },
+      { name: "dscription", content: "注册简书" },
+      { name: "author", content: "戴亮" }
+    ]
+  },
+  data() {}
+};
 </script>
+
+<style scoped>
+
+</style>
